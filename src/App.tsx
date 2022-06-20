c38af15739176717d9dc4479736139463c163b5e
Michael Urich <80188367+mturich@users.noreply.github.com>
2022-06-20 22:37:11 +0200

    tickspeed buy and cost increase implemented, factor bump prep

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,25 @@ import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Dimension from './Dimension';
 
-
 type DimProps = {
    balance: number;
    dimCount: number;
    setDimCount: (fn: (dim: number) => void) => void;
    setBalance: (fn: ((balance: number) => void) | number) => void;
    price: number;
-
 };
 
 function App() {
    const timerExpiredCallback = useRef(() => {});
-   const [balance, setBalance] = useState(100);
-   const [clockSpeed, setClockSpeed] = useState(2000)
+   const clockSpeedRef = useRef(2000);
+   const [tickspeedPrice, setTickspeedPrice] = useState(10);
+   const [balance, setBalance] = useState(200);
    const [firstDimCount, setFirstDimCount] = useState(0);
+   const [fistDimPrice, setFirstDimPrice] = useState(10);
    const [secondDimCount, setSecondDimCount] = useState(0);
+   const [secondDimPrice, setSecondDimPrice] = useState(100)
    const [thirdDimCount, setThirdDimCount] = useState(0);
+   const [thirdDimPrice, setThirdDimPrice] = useState(1000)
    // if the component updates, replace the timeout callback with one that references the new
    //   values of `count` and `firstDimCount`
 
@@ -29,22 +31,24 @@ function App() {
    };
 
    useEffect(() => {
-      // console.log(firstDimCount, secondDimCount, thirdDimCount);
-   });
-
-   useEffect(() => {
       let timeID = -1;
       const startTimer = () => {
          timeID = setTimeout(() => {
             timerExpiredCallback.current();
             startTimer();
-         }, 2000);
+         }, clockSpeedRef.current);
       };
       startTimer();
 
       // if we ever unmount / destroy this component instance, clear the timeout
       return () => clearTimeout(timeID);
    }, []);
+
+   const handleTickBtnClick = () => {
+      clockSpeedRef.current = clockSpeedRef.current * (1 - 0.11);
+      setBalance(prevPrice => prevPrice - tickspeedPrice);
+      setTickspeedPrice(prevPrice => prevPrice * 10);
+   };
 
    return (
       <div className='App'>
@@ -53,14 +57,31 @@ function App() {
                You have {balance % 1 === 0 ? balance.toFixed(0) : balance.toFixed(1)} antimatters.
             </p>
          </div>
-         
+
+         <div className='gridContainer3Rows'>
+            <p className='centered'>{`The current clockspeed is ${clockSpeedRef.current} ms. Reduce the tickspeed by 11%.`}</p>
+            <div className='centered'>
+               <button
+                  className='btn centered'
+                  onClick={handleTickBtnClick}
+                  disabled={balance - tickspeedPrice <= 0}>
+                  Cost of 1: {tickspeedPrice}{' '}
+               </button>
+               <button className='btn centered' disabled={balance - tickspeedPrice <= 0}>
+                  Buy Max
+               </button>
+            </div>
+         </div>
+
          <Dimension
             balance={balance}
             dimCount={firstDimCount}
             setDimCount={setFirstDimCount}
             setBalance={setBalance}
-            price={10}>
-            First Dim
+            price={fistDimPrice}
+            setPrice={setFirstDimPrice}
+         >
+            {`1.Dim Cost: ${fistDimPrice}`}
          </Dimension>
 
          <Dimension
@@ -68,8 +89,9 @@ function App() {
             dimCount={secondDimCount}
             setDimCount={setSecondDimCount}
             setBalance={setBalance}
-            price={100}>
-            Second Dim
+            price={secondDimPrice}
+            setPrice={setSecondDimPrice}>
+            {`2.Dim Cost: ${secondDimPrice}`}
          </Dimension>
 
          {secondDimCount > 0 && (
@@ -78,8 +100,9 @@ function App() {
                dimCount={thirdDimCount}
                setDimCount={setThirdDimCount}
                setBalance={setBalance}
-               price={1000}>
-               Third Dim
+               price={thirdDimPrice}
+               setPrice={setThirdDimPrice}>
+               {`2.Dim Cost: ${thirdDimPrice}`}
             </Dimension>
          )}
       </div>
--- src/Dimension.tsx
+++ src/Dimension.tsx
@@ -10,30 +10,38 @@ type DimProps = {
    children: string;
 };
 export default function Dimension(props: DimProps) {
-   const { balance, dimCount, setDimCount, setBalance, price } = props;
+   const { balance, dimCount, setDimCount, setBalance, price, setPrice } = props;
 
-   const handleDimBuy = (quantity:number) => {
+   const handleDimBuy = (quantity: number) => {
       if (balance >= price) {
          setDimCount(dimCount => dimCount + quantity);
-         setBalance(balance => balance - price*quantity);
+         setBalance(balance => balance - price * quantity);
       }
+      if ((dimCount + 1) % 10 === 0 && dimCount > 1) setPrice(prevPrice => prevPrice * 10);
    };
    return (
       <>
          <br />
-         <div className='gridContainer'>
+         <div className='gridContainer4Cols'>
             <p className='centered'>
-               {`You have ${dimCount%1===0 ? dimCount.toFixed(0): dimCount.toFixed(1)} ${props.children.split(' ')[0]}
+               {`You have ${dimCount % 1 === 0 ? dimCount.toFixed(0) : dimCount.toFixed(1)} ${
+                  props.children.split(' ')[0]
+               }
                ${price / 10}x`}
             </p>
             <p className='centered'>
-               {`You have ${dimCount%1===0 ? dimCount.toFixed(0): dimCount.toFixed(1)} ${props.children.split(' ')[0]}
-               ${price / 10}x`}
+               {` ${dimCount % 1 === 0 ? dimCount.toFixed(0) : dimCount.toFixed(1)}
+                  (${dimCount.toFixed(0) % 10})
+               
+               `}
             </p>
-            <button className='btn' onClick={()=> handleDimBuy(1)} disabled={balance < price}>
+            <button className='btn' onClick={() => handleDimBuy(1)} disabled={balance < price}>
                {props.children}
             </button>
-            <button className='btn' onClick={()=> handleDimBuy(10)} disabled={balance < 10*price}>
+            <button
+               className='btn'
+               onClick={() => handleDimBuy(10)}
+               disabled={balance < 10 * price}>
                Buy 10x {props.children}
             </button>
          </div>
