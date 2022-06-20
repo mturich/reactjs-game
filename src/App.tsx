30c4301d25a4f4d27b08e205bcb940be9eb93a2a
Michael Urich <80188367+mturich@users.noreply.github.com>
2022-06-20 21:21:28 +0200

    added 2. button to buy 10 x Dim

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,87 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
+import Dimension from './Dimension';
+
+
+type DimProps = {
+   balance: number;
+   dimCount: number;
+   setDimCount: (fn: (dim: number) => void) => void;
+   setBalance: (fn: ((balance: number) => void) | number) => void;
+   price: number;
+
+};
 
 function App() {
-   const [count, setCount] = useState(0);
+   const timerExpiredCallback = useRef(() => {});
+   const [balance, setBalance] = useState(100);
+   const [clockSpeed, setClockSpeed] = useState(2000)
    const [firstDimCount, setFirstDimCount] = useState(0);
-   const [timer, setTime] = useState(0);
+   const [secondDimCount, setSecondDimCount] = useState(0);
+   const [thirdDimCount, setThirdDimCount] = useState(0);
+   // if the component updates, replace the timeout callback with one that references the new
+   //   values of `count` and `firstDimCount`
+
+   timerExpiredCallback.current = () => {
+      setBalance(prevBalance => prevBalance + firstDimCount);
+      setFirstDimCount(prevFirstDim => prevFirstDim + secondDimCount / 10);
+      setSecondDimCount(prevSecondDim => prevSecondDim + thirdDimCount / 100);
+   };
 
    useEffect(() => {
+      // console.log(firstDimCount, secondDimCount, thirdDimCount);
+   });
+
+   useEffect(() => {
+      let timeID = -1;
       const startTimer = () => {
-         const myTimer = setTimeout(() => {
-            console.log(count, firstDimCount);
-            setCount(count => count + firstDimCount);
+         timeID = setTimeout(() => {
+            timerExpiredCallback.current();
             startTimer();
-         }, 1000);
+         }, 2000);
       };
-   
-    startTimer();
-      return () => {};
+      startTimer();
+
+      // if we ever unmount / destroy this component instance, clear the timeout
+      return () => clearTimeout(timeID);
    }, []);
 
    return (
       <div className='App'>
-         <div className='score'>You have {count} antimatters.</div>
-         <div>You have {firstDimCount} First Dimension x2.0</div>
-         <button onClick={() => setFirstDimCount(firstDimCount + 1)}>Buy Dimension</button>
+         <div className='heading'>
+            <p className='centered highlight'>
+               You have {balance % 1 === 0 ? balance.toFixed(0) : balance.toFixed(1)} antimatters.
+            </p>
+         </div>
+         
+         <Dimension
+            balance={balance}
+            dimCount={firstDimCount}
+            setDimCount={setFirstDimCount}
+            setBalance={setBalance}
+            price={10}>
+            First Dim
+         </Dimension>
+
+         <Dimension
+            balance={balance}
+            dimCount={secondDimCount}
+            setDimCount={setSecondDimCount}
+            setBalance={setBalance}
+            price={100}>
+            Second Dim
+         </Dimension>
+
+         {secondDimCount > 0 && (
+            <Dimension
+               balance={balance}
+               dimCount={thirdDimCount}
+               setDimCount={setThirdDimCount}
+               setBalance={setBalance}
+               price={1000}>
+               Third Dim
+            </Dimension>
+         )}
       </div>
    );
 }
--- src/Dimension.tsx
+++ src/Dimension.tsx
@@ -1,31 +1,43 @@
 import { useState, useEffect } from 'react';
+import './Dimentions.css';
 
 type DimProps = {
    balance: number;
    dimCount: number;
    setDimCount: (fn: (dim: number) => void) => void;
    setBalance: (fn: ((balance: number) => void) | number) => void;
-   factor: number;
+   price: number;
    children: string;
 };
 export default function Dimension(props: DimProps) {
-   const { balance, dimCount, setDimCount, setBalance, factor } = props;
+   const { balance, dimCount, setDimCount, setBalance, price } = props;
 
-   const handleFirstDimBuy = () => {
-      if (balance >= factor) {
-         setDimCount(dimCount => dimCount + 1);
-         setBalance(balance => balance - factor);
+   const handleDimBuy = (quantity:number) => {
+      if (balance >= price) {
+         setDimCount(dimCount => dimCount + quantity);
+         setBalance(balance => balance - price*quantity);
       }
    };
    return (
       <>
          <br />
-         <div>
-            You have {dimCount} {props.children.split(" ")[0]} {factor / 10}x
+         <div className='gridContainer'>
+            <p className='centered'>
+               {`You have ${dimCount%1===0 ? dimCount.toFixed(0): dimCount.toFixed(1)} ${props.children.split(' ')[0]}
+               ${price / 10}x`}
+            </p>
+            <p className='centered'>
+               {`You have ${dimCount%1===0 ? dimCount.toFixed(0): dimCount.toFixed(1)} ${props.children.split(' ')[0]}
+               ${price / 10}x`}
+            </p>
+            <button className='btn' onClick={()=> handleDimBuy(1)} disabled={balance < price}>
+               {props.children}
+            </button>
+            <button className='btn' onClick={()=> handleDimBuy(10)} disabled={balance < 10*price}>
+               Buy 10x {props.children}
+            </button>
          </div>
-         <button onClick={handleFirstDimBuy} disabled={balance < factor}>
-            {props.children}
-         </button>
+
          <br />
       </>
    );
